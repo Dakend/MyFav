8e6d311b7bcc92769e5f08f28982d551e7267311
Dakend <alc92701@gmail.com>
2020-11-08 15:54:47 +0900

    create Post model

---
+++ app/models/post.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)
+#  title      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+class Post < ApplicationRecord
+  belongs_to :user
+  belongs_to :movie_list
+  validates :title, presence: true
+  validates :user_id, presence: true
+  # validates :movie_list_id, presence: true
+end
