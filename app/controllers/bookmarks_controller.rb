ec0562333aa5d52ba72566710e2b115d3de35e66
Dakend <alc92701@gmail.com>
2020-11-19 11:37:00 +0900

    add create/destroy action in bookmarks_controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,7 +1,10 @@
 class BookmarksController < ApplicationController
   def create
+    bookmark = current_user.bookmarks.build(post_id: params[:post_id])
+    bookmark.save
   end
 
   def destroy
+    current_user.bookmarks.build(post_id: params[:post_id]).destroy!
   end
 end
