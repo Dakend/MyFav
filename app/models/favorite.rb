e11a6768ee1d0c8816964ecc7ac914af5e056cce
Dakend <alc92701@gmail.com>
2020-11-20 20:07:52 +0900

    create favorite-partial-template and apply ajax

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -23,4 +23,8 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :post
   validates_uniqueness_of :post_id, scope: :user_id
+
+  def get_post
+    Post.find(self.post_id)
+  end
 end
