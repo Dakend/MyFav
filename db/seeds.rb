7179903a3dfa91fa7422db4113936a82e42ac58b
Dakend <alc92701@gmail.com>
2021-01-12 01:20:39 +0900

    delete code and add code to read csv

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,35 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'csv'
+
 CSV.foreach('db/csv/seed_users.csv', headers: true) do |row|
-  Brand.create(
+  User.create(
     name: row['name'],
-    image: row['image'],
+    email: row['email'],
+    password: row['password'],
+    profile: row['profile']
   )
+end
+
+(1..4).each do |n|
+  user = User.find(n)
+  user.icon.attach(io: File.open("app/assets/images/users/user_#{n}_icon.jpg"), filename: "user_#{n}_icon.jpg")
+end
+
+CSV.foreach('db/csv/seed_posts.csv', headers: true) do |row|
+  post = PostForm.new(
+    post_title: row['post_title'],
+    post_content: row['post_content'],
+    movie_link_1: row['movie_link_1'],
+    movie_content_1: row['movie_content_1'],
+    movie_link_2: row['movie_link_2'],
+    movie_content_2: row['movie_content_2'],
+    movie_link_3: row['movie_link_3'],
+    movie_content_3: row['movie_content_3'],
+    movie_link_4: row['movie_link_4'],
+    movie_content_4: row['movie_content_4'],
+    user_id: row['user_id']
+  )
+  post.save
 end
